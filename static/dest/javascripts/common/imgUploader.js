7eb0c3c633363fabccf9146676e38284173d4094
Sergey Razin <900556@mail.ru>
2019-03-18 23:40:35 +0400

    добавил картинки к статьям

--- static/dest/javascripts/common/imgUploader.js
+++ static/dest/javascripts/common/imgUploader.js
@@ -6,30 +6,7 @@ $('#formUploadImg').submit(function (event) {
 
     let formData = new FormData(this); //специальный тип данных для формы - есть в каждом браузере
 
-
-    function uploadFile(formData) {
-
-        let promise = new Promise((resolve, reject) => {
-            $.ajax({
-                type: 'POST',
-                url: '/uploadArticleImg',
-                data: formData,
-                processData: false,
-                contentType: false,
-                success: (data) => {
-                    resolve(data);
-                },
-                error: (err) => {
-                    reject(err);
-                }
-            });
-        });
-
-        return promise;
-    }
-
-
-    uploadFile(formData)
+    new EditArticleLoader().uploadFile(formData)
         .then((data) => {
             if(data.ok){
                 //отобразить имя картинки на странице, чтобы пользователь мог его использовать
